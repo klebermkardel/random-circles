f5a3f9e625734f11c2236f07412d3ed1a53784ca
Kleber Molina Kardel <molinakardel@gmail.com>
2024-01-21 17:49:17 -0300

    :sparkles: feat: Adiciona bolinhas estilizadas com tamanho e cor aleatórios
    - Atualiza a renderização dos pontos clicados na interface.
    
    - Adiciona funcionalidade para gerar tamanhos e cores aleatórios para cada bolinha adicionada.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,61 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 interface ClickedProps {
-  clientX: number
-  clientY: number
+  clientX: number;
+  clientY: number;
+  size: number;
+  color: string;
+}
+
+function getRandomColor(): string {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function getRandomSize(): number {
+  return Math.floor(Math.random() * 20) + 10; // Tamanho aleatório entre 10 e 30 pixels
 }
 
 function App() {
-  const [clickedPoints, setClickedPoints] = useState<ClickedProps[]>([])
+  const [clickedPoints, setClickedPoints] = useState<ClickedProps[]>([]);
 
   function getCoordinates(e: React.MouseEvent<HTMLElement>) {
-    const { clientX, clientY } = e
+    const { clientX, clientY } = e;
 
-    setClickedPoints([...clickedPoints, { clientX, clientY }])
+    const newClickedPoint: ClickedProps = {
+      clientX,
+      clientY,
+      size: getRandomSize(),
+      color: getRandomColor(),
+    };
+
+    setClickedPoints([...clickedPoints, newClickedPoint]);
   }
 
-  return <div className='App' onClick={getCoordinates}>
-    {clickedPoints.map((clickedPoint) => {
-      return <div style={{
-        left: clickedPoint.clientX - 7, 
-        top: clickedPoint.clientY - 7,
-        position: 'absolute',
-        borderRadius: '50%',
-        background: 'blue',
-        width: '20px',
-        height: '20px'}}
+  return (
+    <div className='App' onClick={getCoordinates}>
+      {clickedPoints.map((clickedPoint, index) => (
+        <div
+          key={index}
+          style={{
+            left: clickedPoint.clientX - clickedPoint.size / 2,
+            top: clickedPoint.clientY - clickedPoint.size / 2,
+            position: 'absolute',
+            borderRadius: '50%',
+            background: clickedPoint.color,
+            width: `${clickedPoint.size}px`,
+            height: `${clickedPoint.size}px`,
+            opacity: '50%'
+          }}
         ></div>
-    })}
-  </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
